452d623b06b5c1307f4e7b63ffc9eb3f65dddc34
Augusto de Souza <augusto23sgs@gmail.com>
2024-01-18 13:05:57 -0300

    commit files

---
+++ main.js
@@ -0,0 +1,38 @@
+const form = document.getElementById('form-contato');
+const contatos = [];
+const numeros = [];
+
+let linhas = '';
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    adicionaLinha();
+    atualizaTabela();
+});
+
+function adicionaLinha() {
+    const inputNomeContato = document.getElementById('nome-contato');
+    const inputNumeroContato = document.getElementById('numero-contato');
+
+    if (contatos.includes(inputNomeContato.value)) {
+        alert(`O: ${inputNomeContato.value} já foi salvo na lista de contatos`);
+    } else {
+        contatos.push(inputNomeContato.value);
+        numeros.push(inputNumeroContato.value);
+
+        let linha = '<tr>';
+        linha += `<td>${inputNomeContato.value}</td>`;
+        linha += `<td>${inputNumeroContato.value}</td>`;
+        linha += '</tr>';
+
+        linhas += linha;
+    }
+    inputNomeContato.value = '';
+    inputNumeroContato.value = '';
+}
+
+function atualizaTabela() {
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+}
